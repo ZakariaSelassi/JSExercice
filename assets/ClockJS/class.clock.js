689845bdbbe4fc71ba44f512e4fb56128b7742e3
zakaria <zakariaselassi@gmail.com>
2021-03-19 14:54:53 +0100

    update

--- assets/ClockJS/class.clock.js
+++ assets/ClockJS/class.clock.js
@@ -3,31 +3,27 @@ class clocking {
         this.date = new Date();
         this.hrs = this.date.getHours();
         this.mins = this.date.getMinutes();
-        this.second = this.date.getSeconds();
         this.period = "AM";
     }
 
     getHours() {
-        if (this.hrs == 0) {
-            this.hrs = 12;
-        } else if (this.hrs >= 12) {
-            this.hrs = this.hrs - 12;
-            this.period = "PM";
+        let word = '';
+        let switchDayNight = document.getElementById();
+        if (this.hrs >= 6 && this.hrs <= 17) {
+            word = "Good Morning";
+        } else if (this.hrs > 18 && this.hrs < 6) {
+            word = "Good evening";
         }
-        this.hrs = this.hrs < 10 ? "0" + this.hrs : this.hrs;
-        this.mins = this.mins < 10 ? "0" + this.mins : this.mins;
-        this.second = this.second < 10 ? "0" + this.second : this.second;
+
     }
     UpdateHours() {
         this.date = new Date();
         this.hrs = this.date.getHours();
         this.mins = this.date.getMinutes();
-        this.second = this.date.getSeconds();
-        this.period = "AM";
     }
     display() {
 
-        let time = `${this.hrs}:${this.mins}:${this.second} ${this.period}`;
+        let time = `${this.hrs}:${this.mins}`;
         document.getElementById("clock").innerText = time;
         this.UpdateHours();
 
--- assets/ClockJS/clock.js
+++ assets/ClockJS/clock.js
@@ -1,7 +1,42 @@
-let clocker = new clocking();
-setInterval(() => {
-    clocker.display();
-}, 1000);
+let timer = () => {
+    let time = new Date();
+    let hours = time.getHours();
+    let minuts = time.getMinutes();
+    let formatHours = "AM";
+    let message = '';
+    let icon = document.querySelector('img');
+    if (hours >= 0 && hours < 12) {
+        message = 'Good Morning';
+        document.getElementById("timeSentence").innerHTML = `${message}, It's currently`;
+    } else if (hours >= 12 && hours < 18) {
+        message = 'Good afternoon';
+
+        document.getElementById("timeSentence").innerHTML = `${message}, It's currently`;
+    } else {
+        message = 'Good evening';
+        icon.src = './img/icon-moon.svg';
+        icon.setAttribute("alt", "moon");
+        document.getElementById("timeSentence").innerHTML = `${message}, It's currently`;
+    }
+
+    // Fixe 0 missing before 10.
+    minuts = minuts < 10 ? "0" + minuts : minuts;
+    hours = hours < 10 ? "0" + hours : hours;
+    // AM or PM
+
+    if (hours > 12) {
+        hours -= 12;
+        formatHours = "PM";
+    }
+    if (hours == 0) {
+        hours = 12;
+        formatHours = "AM";
+    }
+
+    let timer = `${hours} :${minuts}  ${formatHours}`;
+    document.getElementById("clock").innerHTML = timer;
+}
+timer();
 const quote = document.getElementById("sentence");
 let getSentence = () => {
     fetch('https://api.quotable.io/random')
